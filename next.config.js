8c6573ec7167eb258c71856074d087c11c296441
lesbrokers <lesbrokers@gmail.com>
2026-03-18 09:01:34 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   poweredByHeader: false,
   reactStrictMode: true,
   images: {
     unoptimized: true
   }
 };
+
 module.exports = nextConfig;
